6c6e75a8fdc61e41f673e1047c3aa34a919e2ab0
vladislav <ve@incsub.com>
2024-09-14 05:55:45 +0200

    Write image data to HTML canvas

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,35 @@
+const FACTOR: usize = 5;
+const WIDTH: usize = 16 * FACTOR;
+const HEIGHT: usize = 9 * FACTOR;
+
+
+static mut CANVAS: [u8; WIDTH*HEIGHT*4] = [0; WIDTH*HEIGHT*4];
+
 #[no_mangle]
 pub extern fn add(a: usize, b: usize) -> usize {
     return a+b;
+}
+
+#[no_mangle]
+pub unsafe extern fn get_data() -> u32 {
+    return CANVAS.as_ptr() as u32;
+}
+
+#[no_mangle]
+pub extern fn get_len() -> usize {
+    return WIDTH*HEIGHT*4;
+}
+
+#[no_mangle]
+pub unsafe extern fn init() {
+    for y in 0..HEIGHT {
+        for x in (0..WIDTH).step_by(4) {
+            CANVAS[y * WIDTH + x + 0] = 0xFF;
+            CANVAS[y * WIDTH + x + 1] = 0x11;
+            CANVAS[y * WIDTH + x + 2] = 0x22;
+            CANVAS[y * WIDTH + x + 3] = 0x33;
+        }
+    }
 }
 
 #[cfg(test)]
--- www/index.js
+++ www/index.js
@@ -3,5 +3,17 @@ let WASM;
 async function init() {
 	WASM = await WebAssembly.instantiateStreaming(await fetch("lib.wasm"), {})
 	console.log(WASM)
+
+	WASM.instance.exports.init();
+
+	const buffer_start = WASM.instance.exports.get_data();
+	const buffer_len = WASM.instance.exports.get_len();
+	const pixels = new Uint8ClampedArray(WASM.instance.exports.memory.buffer, buffer_start, buffer_len);
+	const image = new ImageData(pixels, 16*5, 9*5);
+
+	const canvas = document.getElementById("out");
+	const ctx = canvas.getContext("2d");
+	ctx.putImageData(image, 0, 0, 0, 0, 16*5, 9*5)
+
 }
 init();
