0a2c12ca826587235cd1195d9267ca11dd42fb13
vladislav <ve@incsub.com>
2024-09-15 07:14:56 +0200

    Abstract away node projection

--- src/interface.rs
+++ src/interface.rs
@@ -1,3 +1,4 @@
+use super::Area;
 use canvas::{get_default_canvas, Color};
 use map_data::*;
 
@@ -33,66 +34,27 @@ pub unsafe extern "C" fn get_height() -> usize {
 #[no_mangle]
 pub unsafe extern "C" fn init() {
     let bg = Color {
-        r: 0x33,
-        g: 0x33,
-        b: 0x33,
+        r: 0x13,
+        g: 0x32,
+        b: 161,
         a: 0xFF,
     };
     let fg = Color {
-        r: 0,
+        r: 13,
         g: 0xFF,
-        b: 0,
+        b: 13,
         a: 0xFF,
     };
     let mut c = get_default_canvas();
     c.fill(&bg);
 
-    let mut min_lat: f32 = f32::MAX;
-    let mut max_lat: f32 = 0.0;
-    let mut min_lon: f32 = f32::MAX;
-    let mut max_lon: f32 = 0.0;
+    let area = Area::new(&MAP_NODES);
+    let node_size = 5;
     for node in &MAP_NODES {
-        if node.lat > max_lat {
-            max_lat = node.lat;
+        let rel = node.relative_to(&area);
+        if let Some(point) = rel.project_onto(c.get_width(), c.get_height(), node_size) {
+            c.rect(point.x, point.y, node_size, node_size, &fg);
         }
-        if node.lat < min_lat {
-            min_lat = node.lat;
-        }
-        if node.lon > max_lon {
-            max_lon = node.lon;
-        }
-        if node.lon < min_lon {
-            min_lon = node.lon;
-        }
-    }
-
-    let lon_diff = max_lon - min_lon;
-    let lat_diff = max_lat - min_lat;
-    let node_width = 5;
-    for node in &MAP_NODES {
-        let rel_x = (node.lon - min_lon) / lon_diff;
-        let rel_y = (node.lat - min_lat) / lat_diff;
-        let x_px = (rel_x * c.get_width() as f32) as usize;
-        let y_px = (rel_y * c.get_height() as f32) as usize;
-        let mut x0 = if x_px <= node_width / 2 {
-            0
-        } else {
-            (x_px - node_width / 2) - 1
-        };
-        if x0 + node_width > c.get_width() {
-            x0 = c.get_width() - node_width;
-        }
-
-        let mut y0 = if y_px <= node_width / 2 {
-            0
-        } else {
-            (y_px - node_width / 2) - 1
-        };
-        if y0 + node_width > c.get_height() {
-            y0 = c.get_height() - node_width;
-        }
-
-        c.rect(x0, y0, node_width, node_width, &fg);
     }
 }
 
--- src/lib.rs
+++ src/lib.rs
@@ -11,33 +11,167 @@ pub struct Node {
     lon: f32,
 }
 
+impl Node {
+    pub fn rel_x(&self, bounds: &Area) -> f32 {
+        let diff = bounds.diff();
+        (self.lon - bounds.min.lon) / diff.lon
+    }
+
+    pub fn rel_y(&self, bounds: &Area) -> f32 {
+        let diff = bounds.diff();
+        (self.lat - bounds.min.lat) / diff.lat
+    }
+
+    pub fn relative_to(&self, bounds: &Area) -> RelativePoint {
+        RelativePoint {
+            x: self.rel_x(bounds),
+            y: self.rel_y(bounds),
+        }
+    }
+}
+
+pub struct RelativePoint {
+    x: f32,
+    y: f32,
+}
+
+impl RelativePoint {
+    pub fn project_onto(&self, width: usize, height: usize, size: usize) -> Option<AbsolutePoint> {
+        let x = Self::project_dimension(self.x, width, size)?;
+        let y = Self::project_dimension(self.y, height, size)?;
+        Some(AbsolutePoint { x, y })
+    }
+
+    fn project_dimension(src: f32, dest: usize, size: usize) -> Option<usize> {
+        let p0 = (src * dest as f32) as usize;
+        let point = if p0 <= size / 2 {
+            return None;
+        } else {
+            p0 - size / 2
+        };
+        if point + size > dest {
+            return None;
+        }
+        Some(point)
+    }
+}
+
+pub struct AbsolutePoint {
+    x: usize,
+    y: usize,
+}
+
 #[cfg(test)]
-mod test {
+mod points {
+    use super::*;
+
+    #[test]
+    fn relative_to_absolute_no_size() {
+        let p0 = RelativePoint { x: 0.25, y: 0.1 };
+        if let Some(p1) = p0.project_onto(10, 10, 1) {
+            assert_eq!(p1.x, 2);
+            assert_eq!(p1.y, 1);
+        } else {
+            assert!(false);
+        }
+    }
+
+    #[test]
+    fn relative_to_absolute_with_size() {
+        let p0 = RelativePoint { x: 0.25, y: 0.25 };
+        if let Some(p1) = p0.project_onto(10, 10, 3) {
+            assert_eq!(p1.x, 1);
+            assert_eq!(p1.y, 1);
+        } else {
+            assert!(false);
+        }
+    }
+}
+
+pub struct Area {
+    max: Node,
+    min: Node,
+}
+
+impl Area {
+    pub fn new(nodes: &[Node]) -> Self {
+        let mut min = Node {
+            lat: f32::MAX,
+            lon: f32::MAX,
+        };
+        let mut max = Node {
+            lat: f32::MIN,
+            lon: f32::MIN,
+        };
+        for node in nodes {
+            min.lat = min.lat.min(node.lat);
+            min.lon = min.lon.min(node.lon);
+
+            max.lat = max.lat.max(node.lat);
+            max.lon = max.lon.max(node.lon);
+        }
+        Self { min, max }
+    }
+
+    fn diff(&self) -> Node {
+        Node {
+            lat: self.max.lat - self.min.lat,
+            lon: self.max.lon - self.min.lon,
+        }
+    }
+}
+
+#[cfg(test)]
+mod node {
+    use super::*;
     use map_data::MAP_NODES;
 
     #[test]
-    fn node_bounding_box() {
-        let mut min_lat: f32 = f32::MAX;
-        let mut max_lat: f32 = 0.0;
-        let mut min_lon: f32 = f32::MAX;
-        let mut max_lon: f32 = 0.0;
+    fn rel_x() {
+        let b = Area::new(&MAP_NODES);
         for node in &MAP_NODES {
-            if node.lat > max_lat {
-                max_lat = node.lat;
-            }
-            if node.lat < min_lat {
-                min_lat = node.lat;
-            }
-            if node.lon > max_lon {
-                max_lon = node.lon;
-            }
-            if node.lon < min_lon {
-                min_lon = node.lon;
-            }
+            assert!(node.rel_x(&b) >= 0.0, "{}", node.rel_x(&b));
+            assert!(node.rel_x(&b) <= 1.0, "{}", node.rel_x(&b));
         }
-        assert!(min_lat > 0.0);
-        assert!(min_lat < max_lat);
-        assert!(min_lon > 0.0);
-        assert!(min_lon < max_lon);
+    }
+
+    #[test]
+    fn rel_y() {
+        let b = Area::new(&MAP_NODES);
+        for node in &MAP_NODES {
+            assert!(node.rel_y(&b) >= 0.0, "{}", node.rel_y(&b));
+            assert!(node.rel_y(&b) <= 1.0, "{}", node.rel_y(&b));
+        }
+    }
+}
+
+#[cfg(test)]
+mod bounding_box {
+    use super::*;
+    use map_data::MAP_NODES;
+
+    #[test]
+    fn bounds_new() {
+        let b = Area::new(&MAP_NODES);
+
+        assert!(b.min.lat > f32::MIN, "lat: {:?} > {}", b.min, f32::MIN);
+        assert!(b.min.lat < b.max.lat, "lat: {:?} < {:?}", b.min, b.max);
+
+        assert!(b.min.lon > f32::MIN, "lon: {:?} > {}", b.min, f32::MIN);
+        assert!(b.min.lon < b.max.lon, "lon: {:?} < {:?}", b.min, b.max);
+    }
+
+    #[test]
+    fn bounds_diff() {
+        let b = Area::new(&MAP_NODES);
+        let diff = b.diff();
+
+        assert!(diff.lat > 0.0);
+        assert!(diff.lat < b.min.lat, "{} < {}", diff.lat, b.min.lat);
+        assert!(diff.lat < b.max.lat, "{} < {}", diff.lat, b.min.lat);
+
+        assert!(diff.lon > 0.0);
+        assert!(diff.lon < b.min.lon, "{} < {}", diff.lon, b.min.lon);
+        assert!(diff.lon < b.max.lon, "{} < {}", diff.lon, b.max.lon);
     }
 }
