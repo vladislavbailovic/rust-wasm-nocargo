43d432e2f035a6cb9ef8add62d270b0d9d59eaab
vladislav <ve@incsub.com>
2024-09-21 08:57:00 +0200

    Stub out logging outside wasm env

--- src/client.rs
+++ src/client.rs
@@ -1,6 +1,9 @@
+#[cfg(target_arch = "wasm32")]
 extern "C" {
     fn wasm_log(ptr: u32, len: usize);
 }
+#[cfg(not(target_arch = "wasm32"))]
+fn wasm_log(ptr: u32, len: usize) {}
 
 pub fn log(what: &str) {
     unsafe {
